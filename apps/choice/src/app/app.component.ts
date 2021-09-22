2d7d44c82181441232a5cbf0386dc759da4e8757
Brian <brianelblog@gmail.com>
2021-09-22 18:46:27 -0300

    initial commit

---
+++ apps/choice/src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Message } from '@choice/api-interfaces';
+
+@Component({
+  selector: 'choice-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  hello$ = this.http.get<Message>('/api/hello');
+  constructor(private http: HttpClient) {}
+}
