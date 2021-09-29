a2cb0508a8cf47d4fe8486bf7c48114ffe1b7ac5
Brian <brianelblog@gmail.com>
2021-09-29 15:08:54 -0300

    change use paragraph

--- apps/choice/src/app/exams/exam/exam.component.ts
+++ apps/choice/src/app/exams/exam/exam.component.ts
@@ -7,9 +7,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ExamComponent implements OnInit {
 
-  equation: string = '\\sum_{i=1}^nx_i';  
-  paragraph: string = `You can write text, that contains expressions like this: $x ^ 2 + 5$ inside them. As you probably know.`;
-  paragraph2: string = 'You also can write expressions in display mode as follows: $$\\sum_{i=1}^n(x_i^2 - \\overline{x}^2)$$';
+  equation: string = 'Si $\\sum_{i=1}^nx_i$ entonces';
+  paragraph: string = ` $x ^ 2 + 5=7$ .`;
+  paragraph2: string = '$$\\sum_{i=1}^n(x_i^2 - \\overline{x}^2)$$';
 
   constructor() { }
 
