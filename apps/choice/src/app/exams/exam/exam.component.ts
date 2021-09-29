7d54fb355e9052ff0f43b2cd991c808bbbe36d6f
Carduu <jose_101195@hotmail.com>
2021-09-29 13:42:22 -0300

    katex add

--- apps/choice/src/app/exams/exam/exam.component.ts
+++ apps/choice/src/app/exams/exam/exam.component.ts
@@ -3,9 +3,13 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'choice-exam',
   templateUrl: './exam.component.html',
-  styleUrls: ['./exam.component.scss']
+  styleUrls: ['./exam.component.scss'],
 })
 export class ExamComponent implements OnInit {
+
+  equation: string = '\\sum_{i=1}^nx_i';  
+  paragraph: string = `You can write text, that contains expressions like this: $x ^ 2 + 5$ inside them. As you probably know.`;
+  paragraph2: string = 'You also can write expressions in display mode as follows: $$\\sum_{i=1}^n(x_i^2 - \\overline{x}^2)$$';
 
   constructor() { }
 
